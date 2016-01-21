598cbe5b0104d512f0ba7ce1f498ec95068feba6
hiteshlala <hash_lala@yahoo.com>
2016-01-20 20:31:48 -0800

    completes implementation of countNRooksSolutions
    Created two helper functions:
      copySliceBoard - creates a new array of arrays - deeply splices
          think this slows down the calculation significantly
    
      findAllRooksSolutions - does what the name says

--- src/solvers.js
+++ src/solvers.js
@@ -14,19 +14,72 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+window.copySliceBoard = function(array) {
+  var result = [];
+  for(var i = 0; i < array.length; i++) {
+    result.push( array[i].slice(0));
+  }
+  return result;
 };
 
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+window.findNRooksSolution = function(num) {
+  var board = new Board({n:num});
+
+  for(var i = 0; i < board.rows().length; i++) {
+
+   board.togglePiece(i,i);
+
+  }
+
+  return board.rows();
+};
+
+window.findAllRooksSolution = function(num) {
+  // var board = new Board({n:num});
+  var results = [];
+
+  var recurse = function(rows, currentBoard) {
+    currentBoard = currentBoard || new Board({n:num});
+    rows = rows || 0;
+
+    if(rows === num) {
+      results.push(currentBoard);
+    
+    } else {
+
+      for(var col = 0; col < num; col++) {
+        currentBoard.togglePiece(rows, col);
+
+        //if no colisions
+        if( !(currentBoard.hasAnyColConflicts() ||
+              currentBoard.hasAnyRowConflicts()) ) {
+          // recurse to next row using current board
+          var nextBoard = new Board(copySliceBoard(currentBoard.rows()));
+          recurse(rows + 1, nextBoard);
+        }
+
+        currentBoard.togglePiece(rows, col);
+
+      }
+
+    }
+
+  };
+
+  recurse();
+
+  return  results;
+};
+
+
+// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+window.countNRooksSolutions = function(num) {
+
+
+
+  var solutionCount = findAllRooksSolution(num).length;
+  console.log('Number of solutions for ' + num + ' rooks:', solutionCount);
   return solutionCount;
 };
 
